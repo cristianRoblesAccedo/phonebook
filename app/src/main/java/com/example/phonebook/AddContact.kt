4b94f82ad0fcf7e8e20393c4fb94f376670bbd0a
Cristian Robles <cristian.robles@accedo.tv>
2022-02-05 18:49:19 -0600

    EditText fields in AddContact validated using regex

--- app/src/main/java/com/example/phonebook/AddContact.kt
+++ app/src/main/java/com/example/phonebook/AddContact.kt
@@ -22,6 +22,38 @@ class AddContact : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_contact, container, false)
 
+        binding.addBtn.setOnClickListener{ validateInput(it) }
+
         return binding.root
+    }
+
+    fun validateInput(view: View) {
+        val nameLength = Pair(3, 50)
+        val namePattern = """.{3,50}""".toRegex()
+        val emailPattern = """([\w\d_]+\.)+@[\w\d_]+(\.[\w\d]+)+""".toRegex()
+        val phonePattern = """(\(\+?\d{1,3}\)? ?)?((\(?\d{3} ?\)){2}\d{2} ?\d{2}|\(?\d{2}\)? ?\(?\d{4}\)? ?\(?\d{4}\)?)""".toRegex()
+        var nameValid = false
+        var emailValid = false
+        var phoneValid = false
+
+        if (!namePattern.matches(binding.addNameEt.text))
+            binding.addNameEt.error = "name must be between ${nameLength.first} and ${nameLength.second} characters long"
+        else
+            nameValid = true
+
+        if (!emailPattern.matches(binding.addEmailEt.text))
+            binding.addEmailEt.error = "Email is not valid"
+        else
+            emailValid = true
+
+        if (!phonePattern.matches(binding.addPhoneEt.text))
+            binding.addPhoneEt.error = "Phone is not valid"
+        else
+            phoneValid = true
+
+        // If all fields are correct
+        if (nameValid && emailValid && phoneValid) {
+            // TODO: Create a new fragment and add it to ContactList
+        }
     }
 }
